e5d99d228ae21d461df0a926c62fb748874caf20
nrentz-ts <nicolas.rentz@thoughtspot.com>
2025-10-20 16:17:58 -0700

    initial commit

---
+++ actions.js
@@ -0,0 +1,66 @@
+import { Action } from 'https://unpkg.com/@thoughtspot/visual-embed-sdk@1.42.0/dist/tsembed.es.js';
+
+export const actions = [
+  Action.Schedule,
+  Action.SpotIQAnalyze,
+  Action.Share,
+  Action.MakeACopy,
+  Action.AddFilter,
+  Action.Present,
+  Action.Explore,
+  Action.CopyAndEdit,
+  Action.DrillDown,
+  Action.DrillExclude,
+  Action.DrillInclude,
+  Action.DownloadAsPdf,
+  Action.Edit,
+];
+
+export const lbvisibaction = [
+  Action.Download,
+  Action.DownloadAsCsv,
+  Action.DownloadAsPdf,
+  Action.DownloadAsXlsx,
+  Action.Present,
+  Action.AddFilter,
+  Action.Explore,
+  Action.AnswerDelete,
+  Action.MakeACopy,
+  Action.Pin,
+  Action.DrillDown,
+  Action.ShowUnderlyingData,
+  Action.DrillInclude,
+  Action.DrillExclude,
+];
+
+export const drillactions = [
+  Action.DrillDown,
+  Action.DrillExclude,
+  Action.DrillInclude,
+];
+
+export const TMLactions = [
+  Action.ExportTML,
+  Action.ImportTML,
+  Action.UpdateTML,
+  Action.EditTML,
+];
+
+export const MobileActions = [
+  Action.Explore,
+  Action.Share,
+  Action.Present,
+  Action.Schedule,
+  Action.ExportTML,
+  Action.EditTML,
+  Action.ImportTML,
+  Action.UpdateTML,
+  Action.LiveboardInfo,
+  Action.MakeACopy,
+  Action.ShowUnderlyingData,
+  Action.SpotIQAnalyze,
+  Action.Download,
+  Action.DownloadAsCsv,
+  Action.ManagePipelines,
+  Action.SyncToOtherApps,
+];
